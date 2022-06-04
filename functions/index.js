527ef05d6c3fb878516cb22d4b54eed988f57a7a
Tushar <tusharkaramchandain25@gmail.com>
2022-06-04 13:56:02 +0530

    added changes

--- functions/index.js
+++ functions/index.js
@@ -42,20 +42,22 @@ exports.scheduledDelete = functions.pubsub
   });
 
 const setDataToDb = async () => {
+  const dataToPost = {
+    json: true,
+    code: "fortunebirds",
+    scope: "fortunebirds",
+    table: "campaigns",
+  };
+
   const responseFromPost = await axios.post(
     `https://wax.pink.gg/v1/chain/get_table_rows`,
-    {
-      json: true,
-      code: "fortunebirds",
-      scope: "fortunebirds",
-      table: "campaigns",
-      limit: 150,
-    }
+    dataToPost
   );
+
   admin
     .database(app)
     .ref("/campaigns")
-    .once("value", async (snapshot) => {
+    .once("value", (snapshot) => {
       try {
         for (let i = 0; i < responseFromPost.data?.rows?.length; i++) {
           const runningCampaigns = responseFromPost.data?.rows[i];
@@ -64,38 +66,41 @@ const setDataToDb = async () => {
             runningCampaigns?.asset_ids?.length > 0 &&
             snapshot.hasChild(runningCampaigns?.asset_ids[0]) == false
           ) {
-            const response = await axios.get(
-              `https://wax.api.atomicassets.io/atomicassets/v1/assets/${runningCampaigns?.asset_ids[0]}`
-            );
+            axios
+              .get(
+                `https://wax.api.atomicassets.io/atomicassets/v1/assets/${runningCampaigns?.asset_ids[0]}`
+              )
+              .then((response) => {
+                const result = response.data?.data;
 
-            const runningCampaign = {
-              joinedAccounts: runningCampaigns?.accounts || [],
-              assetId: response.data?.data?.asset_id,
-              contractAccount: runningCampaigns?.contract_account,
-              nftImgUrl: `https://ipfs.io/ipfs/${response?.data?.data?.data?.img}`,
-              videoNftUrl: `https://ipfs.io/ipfs/${response?.data?.data?.template?.immutable_data?.video}`,
-              isVideo:
-                `https://ipfs.io/ipfs/${response?.data?.data?.data?.img}` ==
-                true
-                  ? false
-                  : `https://ipfs.io/ipfs/${response?.data?.data?.data?.video}` !=
-                    `https://ipfs.io/ipfs/undefined`
-                  ? true
-                  : false,
-              campaignId: runningCampaigns?.id,
-              creator: runningCampaigns?.authorized_account,
-              entryCost: runningCampaigns?.entrycost,
-              totalEntriesStart: runningCampaigns?.accounts?.length || 0,
-              totalEntriesEnd: runningCampaigns?.max_users,
-              loopTimeSeconds: runningCampaigns?.loop_time_seconds,
-              lastRoll: runningCampaigns?.last_roll,
-              totalEntriesEnd: runningCampaigns?.max_users,
-            };
-
-            admin
-              .database(app)
-              .ref(`/campaigns/${runningCampaigns?.asset_ids[0]}`)
-              .set({ runningCampaign: runningCampaign });
+                const runningCampaign = {
+                  joinedAccounts: runningCampaigns?.accounts || [],
+                  assetId: result?.asset_id,
+                  contractAccount: runningCampaigns?.contract_account,
+                  nftImgUrl: `https://ipfs.io/ipfs/${response?.data?.data?.data?.img}`,
+                  videoNftUrl: `https://ipfs.io/ipfs/${response?.data?.data?.template?.immutable_data?.video}`,
+                  isVideo:
+                    `https://ipfs.io/ipfs/${response?.data?.data?.data?.img}` ==
+                    true
+                      ? false
+                      : `https://ipfs.io/ipfs/${response?.data?.data?.data?.video}` !=
+                        `https://ipfs.io/ipfs/undefined`
+                      ? true
+                      : false,
+                  campaignId: runningCampaigns?.id,
+                  creator: runningCampaigns?.authorized_account,
+                  entryCost: runningCampaigns?.entrycost,
+                  totalEntriesStart: runningCampaigns?.accounts?.length || 0,
+                  totalEntriesEnd: runningCampaigns?.max_users,
+                  loopTimeSeconds: runningCampaigns?.loop_time_seconds,
+                  lastRoll: runningCampaigns?.last_roll,
+                  totalEntriesEnd: runningCampaigns?.max_users,
+                };
+                admin
+                  .database(app)
+                  .ref(`/campaigns/${runningCampaigns?.asset_ids[0]}`)
+                  .set({ runningCampaign: runningCampaign });
+              });
           }
         }
       } catch (error) {
